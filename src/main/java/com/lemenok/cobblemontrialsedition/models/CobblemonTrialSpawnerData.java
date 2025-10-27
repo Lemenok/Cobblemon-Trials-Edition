6287c272beefc8f0a2d56f6424ea63c04839fafd
Lemenok <46767677+Lemenok@users.noreply.github.com>
2025-10-26 20:40:54 -0700

    New Textures for Trial Spawner
    Overhauled New textures for Cobblemon Trial Spawner Block

--- src/main/java/com/lemenok/cobblemontrialsedition/models/CobblemonTrialSpawnerData.java
+++ src/main/java/com/lemenok/cobblemontrialsedition/models/CobblemonTrialSpawnerData.java
@@ -17,6 +17,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
@@ -30,6 +31,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.SpawnData;
@@ -249,7 +251,11 @@ public class CobblemonTrialSpawnerData {
             if (this.displayEntity == null) {
                 CompoundTag compoundTag = this.getOrCreateNextSpawnData(arg, arg2.getRandom()).getEntityToSpawn();
                 if (compoundTag.contains("id", 8)) {
-                    this.displayEntity = EntityType.loadEntityRecursive(compoundTag, arg2, Function.identity());
+                    CompoundTag zombie = new CompoundTag();
+                    zombie.putString("id","minecraft:zombie");
+                    this.displayEntity = EntityType.loadEntityRecursive(zombie, arg2, Function.identity());
+                    /* Item pokeball = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("cobblemon","poke_ball"));
+                    this.displayItem = new ItemStack(pokeball); */
                 }
             }
 
